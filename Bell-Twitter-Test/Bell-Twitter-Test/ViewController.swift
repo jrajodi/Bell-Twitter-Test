e1e7e64cc55582cfc27c63d4a1a25ea3c8968f61
Jignesh Rajodiya <jig.rajodiya@gmail.com>
2019-09-28 01:22:57 -0400

    ### { INITIAL COMMIT }

---
+++ Bell-Twitter-Test/Bell-Twitter-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bell-Twitter-Test
+//
+//  Created by Jigs on 2019-09-28.
+//  Copyright © 2019 Jignesh Rajodiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
